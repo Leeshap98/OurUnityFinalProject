4e5719faa5e5c9e7832a0faa39b6ab2a6ceb719a
OrFrenkelf1 <or.frenkelf1@gmail.com>
2023-06-04 00:20:32 +0300

    fixed

--- Assets/Scripts/PowerUpAnimation.cs
+++ Assets/Scripts/PowerUpAnimation.cs
@@ -9,13 +9,13 @@ public class PowerUpAnimation : MonoBehaviour
 
     void Start()
     {
-        transform.DORotate(new Vector3(0, 360, 0), cycleLength, RotateMode.FastBeyond360)
-            .SetEase(Ease.Linear).SetLoops(-1, LoopType.Restart);
+        //transform.DORotate(new Vector3(0, 360, 0), cycleLength, RotateMode.FastBeyond360)
+            //.SetEase(Ease.Linear).SetLoops(-1, LoopType.Restart);
 
         
 
-        StartCoroutine(WaitToScaleDown());
-        StartCoroutine(WaitFive());
+        //StartCoroutine(WaitToScaleDown());
+        //StartCoroutine(WaitFive());
     }
 
     IEnumerator WaitFive()
