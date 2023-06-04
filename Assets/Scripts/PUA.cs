0c74f9157d8cb8bbe60a1c7c2fdbe5397867873f
OrFrenkelf1 <or.frenkelf1@gmail.com>
2023-06-04 17:58:58 +0300

    Fixed the bugs

--- Assets/Scripts/PUA.cs
+++ Assets/Scripts/PUA.cs
@@ -2,17 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
-
-public class PowerUpAnimations : MonoBehaviour
+public class PUA : MonoBehaviour
 {
     private float cycleLength = 2f;
 
-    void Start()
+    private void Start()
     {
         transform.DORotate(new Vector3(0, 360, 0), cycleLength, RotateMode.FastBeyond360)
             .SetEase(Ease.Linear).SetLoops(-1, LoopType.Restart);
-
-        
 
         StartCoroutine(WaitToScaleDown());
         StartCoroutine(WaitFive());
@@ -23,6 +20,7 @@ public class PowerUpAnimations : MonoBehaviour
         yield return new WaitForSeconds(5);
         Object.Destroy(this.gameObject);
     }
+
     IEnumerator WaitToScaleDown()
     {
         yield return new WaitForSeconds(4);
--- Assets/Scripts/RandomPowerUpsSpawn.cs
+++ Assets/Scripts/RandomPowerUpsSpawn.cs
@@ -4,29 +4,16 @@ using UnityEngine;
 
 public class RandomPowerUpsSpawn : MonoBehaviour
 {
-    private bool createNewPowerUp = false;
-
     public GameObject[] powerUps;
 
     void Update()
     {
-        if(createNewPowerUp == false)
+        if(Input.GetKeyDown(KeyCode.Space))
         {
-            createNewPowerUp = true;
+            int randomIndex = Random.Range(0, powerUps.Length);
+            Vector3 randomSpawnPos = new Vector3(Random.Range(-10, 11), 5, Random.Range(-10, 11));
 
-            StartCoroutine(MakeNewPowerUp());
+            Instantiate(powerUps[randomIndex], randomSpawnPos, Quaternion.identity);
         }
-    }
-
-    IEnumerator MakeNewPowerUp()
-    {
-        yield return new WaitForSeconds(5);
-
-        int randomIndex = Random.Range(0, powerUps.Length);
-        Vector3 randomSpawnPos = new Vector3(Random.Range(-10, 11), 5, Random.Range(-10, 11));
-
-        Instantiate(powerUps[randomIndex], randomSpawnPos, Quaternion.identity);
-
-        createNewPowerUp = false;
     }
 }
